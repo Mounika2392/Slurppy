50740f8a821acf61754e9416ea65ab23557b529e
Mounika <mounika.r36@outlook.com>
2020-04-26 20:42:04 -0400

    Added databinding libraries

---
+++ app/src/main/java/com/learner/slurppy/activties/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.learner.slurppy.activties
+
+import android.os.Bundle
+
+import android.view.Menu
+import android.view.MenuItem
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.learner.slurppy.R
+import com.learner.slurppy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+}
